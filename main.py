51279c6dd7d0a9b63f8a67d7efb62fb1ac5cac36
Yrysbaev <mzamirbekovic02@gmail.com>
2022-05-31 15:24:18 +0600

    added Python GUI

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ win.geometry("400x600")
 win.resizable(False,False)
 win.tk.call('wm', 'iconphoto', win._w, tkinter.PhotoImage(file='Icon.png'))
 
-frame = Frame(win,bg='green')
+frame = Frame(win)
 frame.pack(side="top", expand=True, fill="both")
 
 class HomePage:
     def __init__(self):
-        clear_frame()
-   
-        self.l = Label(frame, text = " WELCOME ")
+        clear_frame()   
+        self.Wel = Label(frame, text = "Welcome to My")
+        self.Pho = Label(frame, text = "Photo Editor")
         self.Text()
         self.image1 = Image.open("Ala-Too_International_University_Seal.png")
         self.Ala_log()
         self.Exit = tkinter.Button(frame, text ="EXIT", command = self.quitwin )
-        self.Spage = tkinter.Button(frame, text ="TEST", command = Spage().Labe )
+        self.Spage = tkinter.Button(frame, text ="Python GUI", command = Spage().Labe )
         self.Second = tkinter.Button(frame, text ="White & Black", command = SecondPage )
         self.Buttons()
     def Text(self):
-        self.l.config(font =("Courier", 14))  
-        self.l.pack()
+        self.Wel.config(font =("Courier", 14))  
+        self.Wel.pack()
+        self.Pho.config(font =("Courier", 14))  
+        self.Pho.pack()
 
     def Buttons(self):
         self.Exit.pack(side=BOTTOM,pady=5)
@@ -81,18 +83,46 @@ class SecondPage:
         self.x = self.image1.height
         self.y = self.image1.width
         self.Image_set()
-
 class Spage:
     def Labe(self):
         clear_frame()
-        Label(frame,text="Enter the Password", font=('Helvetica',20)).pack(pady=20)
-        Button(frame, text="Clear", font=('Helvetica bold', 10), command=HomePage).pack(pady=20)
+        def clear_textbox():
+            text_box.delete(1.0, 'end')
+        message ='''
+        Dear Reader,
 
+            Don't let this situation
+            blind your future. We at
+            PythonGuides write tutorials
+            with real life examples to 
+            make you understand the 
+            concept in best 
+            possible way.
+
+        Thanks & Regards,
+        Team PythonGuides '''
+
+        text_box = Text(
+            frame,
+            height=13,
+            width=40
+        )
+        text_box.pack(expand=True)
+        text_box.insert('end', message)
+
+        Button(
+            frame,
+            text='Clear',
+            width=15,
+            height=2,
+            command=clear_textbox
+        ).pack(expand=True)
+        self.B = Button(frame, text="Home Page", font=('Helvetica bold', 10), command=HomePage)
+        self.B.pack(side=BOTTOM)
 def clear_frame():
     for widgets in frame.winfo_children():
         widgets.destroy()
 
 HomePage()
-test1.Yrysbaev_Maksatbek()
 
 win.mainloop()
